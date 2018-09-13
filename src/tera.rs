a4856b7503482f287296018ad28556ddc327a32d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2018-09-13 17:08:00 +0200

    Adapt to changed Tera API

--- src/tera.rs
+++ src/tera.rs
@@ -19,7 +19,7 @@ pub fn big_table(b: &mut criterion::Bencher, size: &usize) {
     tera.add_raw_templates(vec![("big-table.html", BIG_TABLE_TEMPLATE)])
         .unwrap();
     let mut ctx = Context::new();
-    ctx.add("table", &table);
+    ctx.insert("table", &table);
 
     let _ = tera.render("big-table.html", &ctx).unwrap();
     b.iter(|| tera.render("big-table.html", &ctx));
@@ -43,8 +43,8 @@ pub fn teams(b: &mut criterion::Bencher, _: &usize) {
     tera.add_raw_templates(vec![("teams.html", TEAMS_TEMPLATE)])
         .unwrap();
     let mut ctx = Context::new();
-    ctx.add("year", &2015);
-    ctx.add(
+    ctx.insert("year", &2015);
+    ctx.insert(
         "teams",
         &vec![
             Team {
