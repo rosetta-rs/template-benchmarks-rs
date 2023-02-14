89be1fe15be1191f95c5adba21461de92aa3abd8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-02-14 12:04:34 +0100

    Update dependencies

--- benches/all.rs
+++ benches/all.rs
@@ -1,47 +1,43 @@
-use criterion::{criterion_group, criterion_main, Criterion, Fun};
+use criterion::{criterion_group, criterion_main, Criterion};
 use template_benchmarks_rs::{
-    askama_bench, fomat, handlebars, horrorshow_bench, liquid, markup_bench, maud_bench, ructe, sailfish,
-    std_write, tera,
+    askama_bench, fomat, handlebars, horrorshow_bench, liquid, markup_bench, maud_bench, ructe,
+    sailfish, std_write, tera,
 };
 
 fn big_table(c: &mut Criterion) {
-    c.bench_functions(
-        "Big table",
-        vec![
-            Fun::new("Askama", |b, i| askama_bench::big_table(b, i)),
-            Fun::new("fomat", |b, i| fomat::big_table(b, i)),
-            Fun::new("Handlebars", |b, i| handlebars::big_table(b, i)),
-            Fun::new("Horrorshow", |b, i| horrorshow_bench::big_table(b, i)),
-            Fun::new("Liquid", |b, i| liquid::big_table(b, i)),
-            Fun::new("Markup", |b, i| markup_bench::big_table(b, i)),
-            Fun::new("Maud", |b, i| maud_bench::big_table(b, i)),
-            Fun::new("Ructe", |b, i| ructe::big_table(b, i)),
-            Fun::new("Sailfish", |b, i| sailfish::big_table(b, i)),
-            Fun::new("Tera", |b, i| tera::big_table(b, i)),
-            Fun::new("write", |b, i| std_write::big_table(b, i)),
-        ],
-        100,
-    );
+    let input = 100;
+    let mut group = c.benchmark_group("Big table");
+    group.bench_with_input("Askama", &input, |b, i| askama_bench::big_table(b, i));
+    group.bench_with_input("fomat", &input, |b, i| fomat::big_table(b, i));
+    group.bench_with_input("Handlebars", &input, |b, i| handlebars::big_table(b, i));
+    group.bench_with_input("Horrorshow", &input, |b, i| {
+        horrorshow_bench::big_table(b, i)
+    });
+    group.bench_with_input("Liquid", &input, |b, i| liquid::big_table(b, i));
+    group.bench_with_input("Markup", &input, |b, i| markup_bench::big_table(b, i));
+    group.bench_with_input("Maud", &input, |b, i| maud_bench::big_table(b, i));
+    group.bench_with_input("Ructe", &input, |b, i| ructe::big_table(b, i));
+    group.bench_with_input("Sailfish", &input, |b, i| sailfish::big_table(b, i));
+    group.bench_with_input("Tera", &input, |b, i| tera::big_table(b, i));
+    group.bench_with_input("write", &input, |b, i| std_write::big_table(b, i));
+    group.finish();
 }
 
 fn teams(c: &mut Criterion) {
-    c.bench_functions(
-        "Teams",
-        vec![
-            Fun::new("Askama", |b, i| askama_bench::teams(b, i)),
-            Fun::new("fomat", |b, i| fomat::teams(b, i)),
-            Fun::new("Handlebars", |b, i| handlebars::teams(b, i)),
-            Fun::new("Horrorshow", |b, i| horrorshow_bench::teams(b, i)),
-            Fun::new("Liquid", |b, i| liquid::teams(b, i)),
-            Fun::new("Markup", |b, i| markup_bench::teams(b, i)),
-            Fun::new("Maud", |b, i| maud_bench::teams(b, i)),
-            Fun::new("Ructe", |b, i| ructe::teams(b, i)),
-            Fun::new("Sailfish", |b, i| sailfish::teams(b, i)),
-            Fun::new("Tera", |b, i| tera::teams(b, i)),
-            Fun::new("write", |b, i| std_write::teams(b, i)),
-        ],
-        0,
-    );
+    let input = 0;
+    let mut group = c.benchmark_group("Teams");
+    group.bench_with_input("Askama", &input, |b, i| askama_bench::teams(b, i));
+    group.bench_with_input("fomat", &input, |b, i| fomat::teams(b, i));
+    group.bench_with_input("Handlebars", &input, |b, i| handlebars::teams(b, i));
+    group.bench_with_input("Horrorshow", &input, |b, i| horrorshow_bench::teams(b, i));
+    group.bench_with_input("Liquid", &input, |b, i| liquid::teams(b, i));
+    group.bench_with_input("Markup", &input, |b, i| markup_bench::teams(b, i));
+    group.bench_with_input("Maud", &input, |b, i| maud_bench::teams(b, i));
+    group.bench_with_input("Ructe", &input, |b, i| ructe::teams(b, i));
+    group.bench_with_input("Sailfish", &input, |b, i| sailfish::teams(b, i));
+    group.bench_with_input("Tera", &input, |b, i| tera::teams(b, i));
+    group.bench_with_input("write", &input, |b, i| std_write::teams(b, i));
+    group.finish();
 }
 
 criterion_group!(benches, big_table, teams);
